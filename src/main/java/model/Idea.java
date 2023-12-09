661207695d97d01e797bb53a68314c690bda46f0
Yurii Debeliak <113760917+De-Yurii@users.noreply.github.com>
2023-12-09 18:49:17 +0200

    V1.1 Start functional

--- src/main/java/model/Idea.java
+++ src/main/java/model/Idea.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Idea {
-    public static int ideaAmount;
+    public static int ideaAmount = 1;
     private int ideaID;
     private String ideaText;
     private Client ideaAuthor;
@@ -16,5 +16,44 @@ public class Idea {
         if (ideaID != idea.ideaID) return false;
         if (!ideaText.equals(idea.ideaText)) return false;
         return ideaAuthor.equals(idea.ideaAuthor);
+    }
+
+    public Idea(String ideaText, Client ideaAuthor) {
+        this.ideaID = ideaAmount;
+        this.ideaText = ideaText;
+        this.ideaAuthor = ideaAuthor;
+        ideaAmount++;
+    }
+
+    public static int getIdeaAmount() {
+        return ideaAmount;
+    }
+
+    public static void setIdeaAmount(int ideaAmount) {
+        Idea.ideaAmount = ideaAmount;
+    }
+
+    public int getIdeaID() {
+        return ideaID;
+    }
+
+    public void setIdeaID(int ideaID) {
+        this.ideaID = ideaID;
+    }
+
+    public String getIdeaText() {
+        return ideaText;
+    }
+
+    public void setIdeaText(String ideaText) {
+        this.ideaText = ideaText;
+    }
+
+    public Client getIdeaAuthor() {
+        return ideaAuthor;
+    }
+
+    public void setIdeaAuthor(Client ideaAuthor) {
+        this.ideaAuthor = ideaAuthor;
     }
 }
